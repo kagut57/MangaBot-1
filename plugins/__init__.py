e9c6aef0175b7f86a061d531875be1bafbda7906
kagut57 <156945786+kagut57@users.noreply.github.com>
2024-12-21 23:20:36 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -3,7 +3,7 @@ from .manhuako import ManhuaKoClient
 from .tmo import TMOClient
 from .mangadex import MangaDexClient
 from .mangasin import MangasInClient
-from .mcreader import MgekoClient
+from .mgecko import MgekoClient
 from .mangakakalot import MangaKakalotClient
 from .manganelo import ManganeloClient
 from .manganato import ManganatoClient
