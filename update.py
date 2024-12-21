11df49ecc79e4920b0f0ff15b219b826252accc0
kagut57 <156945786+kagut57@users.noreply.github.com>
2024-12-21 20:30:25 +0530

    Create update.py

---
+++ update.py
@@ -0,0 +1,46 @@
+import os
+import subprocess
+import logging
+import config
+LOGGER = logging.getLogger(__name__)
+
+def gitpull():
+    UPSTREAM_BRANCH = config.UPSTREAM_BRANCH
+    UPSTREAM_REPO = config.UPSTREAM_BRANCH
+    try:
+        if len(UPSTREAM_REPO) == 0:
+            raise TypeError("UPSTREAM_REPO is empty")
+    except TypeError:
+        UPSTREAM_REPO = None
+
+    try:
+        if len(UPSTREAM_BRANCH) == 0:
+            raise TypeError("UPSTREAM_BRANCH is empty")
+    except TypeError:
+        UPSTREAM_BRANCH = 'master'
+
+    if UPSTREAM_REPO is not None:
+        try:
+            if os.path.exists('.git'):
+                subprocess.run(["rm", "-rf", ".git"])
+            
+            update = subprocess.run([
+                f"git init -q "
+                f"&& git config --global user.email kagutsuchi57@outlook.com"
+                f"&& git config --global user.name kagut57"
+                f"&& git add . "
+                f"&& git commit -sm update -q "
+                f"&& git remote add origin {UPSTREAM_REPO} "
+                f"&& git fetch origin -q "
+                f"&& git reset --hard origin/{UPSTREAM_BRANCH} -q"
+            ], shell=True, capture_output=True, text=True)
+            
+            if update.returncode == 0:
+                LOGGER.info('Successfully updated with latest commit from UPSTREAM_REPO')
+            else:
+                LOGGER.warning(f'Update failed. Error: {update.stderr}')
+        
+        except Exception as e:
+            LOGGER.error(f'An error occurred during update: {e}')
+
+gitpull()
