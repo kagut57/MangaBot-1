282956fd1722ade270c534ee9a973c1cbe1c0636
kagut57 <156945786+kagut57@users.noreply.github.com>
2024-12-21 20:35:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import asyncio as aio
+
+from api import run_web_server
 from bot import bot, DB, manga_updater, chapter_creation
 
 
 async def async_main():
     db = DB()
     await db.connect()
+    await run_web_server()
     
 if __name__ == '__main__':
     loop = aio.get_event_loop_policy().get_event_loop()
